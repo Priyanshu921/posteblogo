1e6b765ede1155c2048cfcabc5e9a1ef061b6eae
Priyanshu921 <priyanshupatidar@yahoo.com>
2023-05-15 16:05:38 +0530

    changd static path

--- server/app.js
+++ server/app.js
@@ -2,9 +2,19 @@ import express from "express";
 import cors from 'cors';
 import { routes } from "./Routes/routes";
 export const app = express();
-
+import path from "path"
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 app.use('/api',routes)
 
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(path.resolve() , "/client/build/index.html"));
+  });
+}
+console.log(path.join(path.resolve(), "/client/build/index.html"));
