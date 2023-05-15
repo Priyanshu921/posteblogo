13caa238a4379aff188f21252da1984e664d2025
Priyanshu921 <priyanshupatidar@yahoo.com>
2023-05-15 17:36:51 +0530

    changed navbar heading

--- client/src/Pages/Navbar/Navbar.js
+++ client/src/Pages/Navbar/Navbar.js
@@ -15,7 +15,7 @@ export const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            Navbar
+            Post-e-Blogo
           </Link>
           <button
             className="navbar-toggler"
